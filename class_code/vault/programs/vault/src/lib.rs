6eb66c70d87078ea39494392c203d7ab799ab15c
Hakhyun Kim <kimhakhyun@gmail.com>
2024-05-23 21:54:18 +0900

    in-class vault

--- class_code/vault/programs/vault/src/lib.rs
+++ class_code/vault/programs/vault/src/lib.rs
@@ -7,9 +7,78 @@ pub mod vault {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+        ctx.accounts.initialize(&ctx.bumps)
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user:Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"state"], user.key().as_ref(),
+        bump,
+        space = VaultState::INIT_SPACE,
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump,
+    )]
+    vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        self.vault_state.vault_bump = bumps.vault;
+        self.vault_state.state_bump = bumps.vault_state;
+    };
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump - vault_state.vault_bump,
+    )]
+    pub vault: Account<'info, SYstemAccount>,
+    #[account(
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump,
+    )]
+
+    pub vault_state: SystemAccount<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Deposit<'info> {
+    
+    pub fn depost(&mut self, amout: u64) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.user.to.to_account_info(),
+            to: self.vault.to_account_info(),
+        };
+        let cpi_ctx:  
+
+        tran  
+    }
+
+}
+
+#[account]
+pub struct VaultState {
+    pub vault_bump: u8,
+    pub state_bump: u8,
+}
+
+impl Sapce for VaultState {
+    const INIT_SPACE: usize = 8+1+1;
+}
